e60c615b39ce75d357f3692b0606d40f536bf64d
Marco Guidi <mark.88g@gmail.com>
2020-10-29 15:21:21 +0100

    JS ask users numbers

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,8 @@ $("document").ready(function() {
     var max = 100; // max value random number
     var totNumbers = 5; // total of rnd-n
     var rndNumbers = []; // array of rnd-n
+    var time = 2; // (sec) after that time cards --> hidden
+    var userNumbers = []; // user-numbers
 
     // ALGORITHM
     // STEP 1 numbers generator
@@ -14,12 +16,29 @@ $("document").ready(function() {
     // STEP 2 create card (with numbers) in html
     cardCreator(rndNumbers);
 
-    // STEP 3 hidden card after 30s
-    // STEP 3 ask numbers to user
-    // STEP 4 control user numbers
-    // STEP 5 show correct answer
+    // AFTER "TIME" SEC
+    setTimeout(function() {
+        // STEP 3 hidden the cards
+        hiddenCard();
+
+        $("document").ready(function() {
+            // STEP 4 ask numbers to user
+            userNumbers = askNumbers(min, max, totNumbers);
+            // stamp
+            console.log(userNumbers);
+
+            // userNumbers = askANumber(2, 5);
+
+            // STEP 4 control user numbers
+            // STEP 5 show correct answer
+
+        });
+
+
+    }, (time * 1000));
 
 });
+
 
 // <1-fold SSSSS ALL FUNCTION SSSSS
 
@@ -58,5 +77,41 @@ function cardCreator(numbers) {
     }
 }
 // #/2-fold EEEEE CARD CREATOR EEEEE
+
+// #2-fold SSSSS HIDDEN CARD SSSSS
+// hidden card
+function hiddenCard() {
+    $(".card").addClass("hidden");
+}
+// #/2-fold EEEEE HIDDEN CARD EEEEE
+
+// #2-fold SSSSS ASK A NUMBER SSSSS
+// ask a CORRECT number from min to max INCLUDES
+function askANumber(min, max) {
+    var n; // number
+    do {
+        n = parseInt(prompt("Inserisci un numero:"));
+    } while (!(!isNaN(n) && n >= min && n <= max));
+    return n;
+}
+// #/2-fold EEEEE ASK A NUMBER EEEEE
+
+// #2-fold SSSSS ASK TOT NUMBER SSSSS
+// ask tot CORRECT number from min to max INCLUDES
+function askNumbers(min, max, tot) {
+    var n; // number
+    var array = [];
+    while (tot != array.length) {
+        // ask 1 number
+        n = askANumber(min, max);
+        if (!array.includes(n)) {
+            array.push(n);
+        }
+    }
+
+    return array;
+}
+// #/2-fold EEEEE ASK TOT NUMBER EEEEE
+
 
 // </1-fold> EEEEE ALL FUNCTION EEEEE
